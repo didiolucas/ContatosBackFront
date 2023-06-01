b87282e5b33ea19b80297e6385b77574528af5d8
DidioDev <lucassmaniottoo@gmail.com>
2023-06-01 16:58:20 -0300

    first commit

---
+++ FrontEnd/src/components/Home/Home.jsx
@@ -0,0 +1,149 @@
+//STYLES IMPORTADOS
+import style from "./Home.module.css";
+
+//CARDS IMPORTADOS
+import { CardPerson } from "../User/CardPerson";
+import { CardContato, Delete, Update } from "../Contato/CardContatos";
+
+//ICONS IMPORTADOS
+import { BsFillTrash3Fill } from "react-icons/bs";
+import { IoPencil, IoSearchOutline, IoAdd } from "react-icons/io5";
+import { useEffect, useState } from "react";
+import { api, server } from "../../api/axios";
+
+
+export function Home() {
+
+  let [persons, setPersons] = useState([]);
+  let [valor, setValor] = useState('');
+
+  useEffect(() => {
+    Load()
+  }, []);
+
+  async function Load() {
+    const temp = await server.get("user/");
+
+    setPersons(temp.data);
+    console.log({persons});
+  }
+
+  async function Adicionar() {
+    event.preventDefault();
+
+    const resultAPI = await api.get("/");
+
+    const user = resultAPI.data.results[0];
+
+    const nomeCompleto = `${user.name.first} ${user.name.last}`;
+    const avatar = user.picture.large;
+    const cell = user.cell;
+
+    await server.post("user/", {
+      name: nomeCompleto,
+      avatar: avatar,
+      celular: cell,
+    });
+
+    Load();
+  }
+
+  async function Deletar() {
+    event.preventDefault();
+
+    Delete();
+    Load();
+  }
+
+  function Pesquisar() {
+    event.preventDefault();  
+
+    if (valor === '') { Load(); }
+    else{
+      console.log(`Nome procurado: ${valor}`);
+      let results = [];
+
+    persons.map((person => {
+  
+      if(person.name.toLowerCase().includes(valor.toLowerCase())) {
+        results.push(person);
+        return
+      }
+    }))
+
+    console.log({results})
+    setPersons(results)
+    }
+  }
+
+  async function Editar() {
+    event.preventDefault();
+
+    const resultAPI = await api.get("/");
+
+    const user = resultAPI.data.results[0];
+
+    Update(user);
+    Load();
+  }
+
+  return (
+    <div className={style.container}>
+
+      <CardPerson
+        cover="https://static.vecteezy.com/ti/fotos-gratis/p3/3343387-desktop-source-code-and-wallpaper-by-coding-and-programming-gratis-foto.jpg"
+        avatar="https://avatars.githubusercontent.com/u/96553782?v=4"
+        name="Lucas Smaniotto"
+        office='Developer' />
+
+      <div className={style.contatos}>
+        <div className={style.controles}>
+          <form >
+
+            <div className={style.headerContatos}>
+              <h1 className={style.title}>Meus Contatos:</h1>
+              <div className={style.buttons}>
+                <button className={style.buttomAdd} onClick={Adicionar}> <IoAdd /> </button>
+                <button className={style.buttomEdit} onClick={Editar}> <IoPencil /> </button>
+                <button className={style.buttomRemove} onClick={Deletar}> <BsFillTrash3Fill /> </button>
+
+              </div>
+            </div>
+
+            <div className={style.pesquisa}>
+              <button className={style.buttomPesquisa} onClick={Pesquisar}> <IoSearchOutline /> </button>
+
+              <input type='text' name='pesquisa' className={style.inputPesquisa} placeholder="Procure pelo nome de contato." value={valor} onChange={() => { setValor(event.target.value) }} />
+            </div>
+
+          </form>
+        </div>
+
+        <div className={style.listaContatos}>
+
+          <div className={style.listaCatalogo}>
+            <h1 className={style.letraCatalogo}>All</h1>
+            <div className={style.contatoCatalogo}>
+
+              {
+                persons.map((person, index) => (
+                  <CardContato key={index}
+                    avatar={person.avatar}
+                    name={person.name}
+                    celular={person.celular}
+
+                    id={person.id}
+                  />
+
+                ))
+              }
+
+            </div>
+          </div>
+
+
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ FrontEnd/src/components/User/CardPerson.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import stylePerson from "./CardPerson.module.css";
+
+export function CardPerson({ cover, avatar, name, office, stacks }) {
+  return (
+    <div className={stylePerson.card}>
+      <img src={cover} alt="Cover" />
+      <img className={stylePerson.avatar} src={avatar} alt="Avatar" />
+      <p className={stylePerson.p1}>{name}</p>
+      <p className={stylePerson.p2}>{office}</p>
+
+      <div className={stylePerson.spans}>
+      <span>#HTML/CSS</span>
+      <span>#JavaScript</span>
+      <span>#Eluna</span>
+      </div>
+      
+    </div>
+  );
+}
